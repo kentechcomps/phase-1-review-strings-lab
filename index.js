a3d10898c2bce534c80c2a74bcabe54ce8130940
user <mutukukennedy34@gmail.com>
2023-06-12 22:47:27 +0300

    firsttests

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Write your code in this file!
+const currentUser = 'Grace Hopper';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser} !`;
+const shortGreeting = `Welcome, ${currentUser.splice()} !`
+
+
+
+
+
+
+//const currentUser = 'KennedyMutuku'
+//const welcomeMessage = "Welcome to Flatbook, !"
+//const welcomeMessage = 'Welcome to Flatbook, ${currentUser} !' ;
+//const excitedWelcomeMessage = "WELCOME TO FLATBOOK" + currentUser + !";
+//const shortGreeting = "Welcome," + currentUser.slice(0) + "!";
+//const shortGreeting = `Welcome to Flatbook, ${currentUser} !`
